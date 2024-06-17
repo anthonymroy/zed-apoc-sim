a555cbaaa6e16bf9cd3d46f1faa97ffa9da1a466
anthonymroy <tonyroy46@yahoo.com>
2024-06-17 15:13:59 -0700

    Adding instructions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 import config
+import errno
+import os
 import pickle
 import setup
 import simulate
@@ -10,17 +12,21 @@ MODE = "state"
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser()    
     parser.add_argument("--sim", dest="sim_only", action="store_true", default=False,
-                           help="Simulation only")
+                           help="Flag to run only the simulation without visualization")
     parser.add_argument("--viz", dest="viz_only", action="store_true", default=False,
-                           help="process the full dataset")
+                           help="Flag to visualize the last simulation without rerunning it")
     return parser.parse_args()    
 
 if __name__ == "__main__":
     my_args = parse_arguments()
     
     shape_gdf, border_df, population_df = setup.main(MODE) 
-    
+    my_args.viz_only = True
     if my_args.viz_only:
+        if not os.path.exists(config.LAST_SIMULATION_FILENAME):
+            e = FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), config.LAST_SIMULATION_FILENAME)
+            e.strerror += ". Run the simulation locally to create it" #Add remedy to error message
+            raise e
         time_data = pickle.load(open(config.LAST_SIMULATION_FILENAME, "rb"))
     else: 
         data_df = simulate.initialize(shape_gdf, border_df, population_df)    
